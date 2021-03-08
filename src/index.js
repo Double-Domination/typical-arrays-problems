7f506dd863e8a6f6bbe00ec42e5f74c8e73220e5
Double-Domination <squremeta@gmail.com>
2021-03-08 23:16:00 +0700

    init: task typical array problems start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) !== true || array.length === 0) {
+        return 0;
+    } else {
+        let minimum = Infinity;
+        for (const current of array) {
+            if (current < minimum) {
+                minimum = current;
+            }
+        }
+        return minimum;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) !== true || array.length === 0) {
+        return 0;
+    } else {
+        let maximum = -Infinity;
+        for (const current of array) {
+            if (current > maximum) {
+                maximum = current;
+            }
+        }
+        return maximum;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) !== true || array.length === 0) {
+        return 0;
+    } else {
+        let result = 0;
+        for (const current of array) {
+            result = result + current;
+        }
+        result = result / array.length;
+        return result;
+    }
+};
